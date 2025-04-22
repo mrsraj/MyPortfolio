97fb12c2894a9005fbc3993e556125e5c07159d4
Sachin Raj <sachinraj6392@gmail.com>
2025-04-23 01:22:05 +0530

    add linkedin resume github

--- src/Data/Bio.js
+++ src/Data/Bio.js
@@ -5,10 +5,9 @@ const Bio = {
     ],
     description:
         "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/rishavchanda",
-    resume:
-        "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-    // linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    github: "https://github.com/mrsraj?tab=repositories",
+    resume: "https://drive.google.com/file/d/12OaF1gqU56m-V8l2SqRIFX6ctW6n9jej/view?usp=drivesdk",
+    linkedin: "https://www.linkedin.com/in/sachin-raj-953295267/",
     // twitter: "https://twitter.com/RishavChanda",
     // insta: "https://www.instagram.com/rishav_chanda/",
     // facebook: "https://www.facebook.com/rishav.chanda.165/",
