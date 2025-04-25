0ab757fce4b39ca5eb8e3135fa57410933a0e36e
Sachin Raj <sachinraj6392@gmail.com>
2025-04-25 23:43:42 +0530

    Modify in css

--- src/Components/HeroSection/HeroStyle.js
+++ src/Components/HeroSection/HeroStyle.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import _default from "../../thems/default";
+import _default from "../../Thems/default";
 
 export const HeroContainer = styled.div`
   background: ${({ theme }) => theme.card_light};
