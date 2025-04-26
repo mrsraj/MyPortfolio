bd4657630804c1a052aa41802e712996b53254df
Sachin Raj <sachinraj6392@gmail.com>
2025-04-26 23:25:04 +0530

    Add project image

--- src/Data/Project.js
+++ src/Data/Project.js
@@ -5,7 +5,7 @@ const projects = [
         date: "July 2024 - December 2024",
         description:
             " Used for task management, project tracking, and team collaboration. Features include drag-and-drop card movement, customizable Task, customizable Task , customizable boards,  and cards.",
-        //image:"https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+        image:"image/wt.png",
         tags: ["React Js", "ContextAPI", "ReactQuery", "MySQL", "Django", "Django Rest Framework", "JavaScript", "HTML", "CSS"],
         category: "Web App",
         github_UI: "https://github.com/mrsraj/Work-Tracking",
@@ -32,7 +32,7 @@ const projects = [
         //date: "Oct 2022 - Present",
         description:
             "I designed and created a user-friendly website that allows users to search for recipes by food type or name.",
-        //image: "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+        image: "image/recipefinder.png",
         tags: [
             "JavaScript",
             "HTML",
@@ -50,7 +50,7 @@ const projects = [
         //date: "jan 2025 - Mar 2025",
         description:
             "A coaching portfolio website that highlights offered courses, course introduction videos, and faculty expertise. It provides detailed insights into programs, student testimonials, and easy contact options for inquiries and enrollment.",
-        //image:"https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+        image:"image/coaching.png",
         tags: ["React.js","ContextAPI","React Router","Javascript","HTML","CSS"],
         category: "Website",
         github: "https://github.com/mrsraj/Coaching-Portfolio",
@@ -76,7 +76,7 @@ const projects = [
         //date: "Dec 2021 - Apr 2022",
         description:
             "A modern restaurant website showcasing the menu, chef specials, and ambiance through high-quality images and videos. It includes online table booking, location details, and customer reviews for a complete dining experience.",
-        //image:"https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+        image:"image/resturant.png",
         tags: ["React.js","JavaScript", "ContextAPI", "CSS Module", "HTML"],
         category: "Website",
         github: "https://github.com/rishavchanda/Buckoid-Android-App",
